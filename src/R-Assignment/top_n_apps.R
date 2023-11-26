8e42b2571cb1969f4654a29898fc6b9711e55788
SandSide <37347875+SandSide@users.noreply.github.com>
2023-11-26 01:05:20 +0000

    feat: graph top installed categories

--- src/R-Assignment/top_n_apps.R
+++ src/R-Assignment/top_n_apps.R
@@ -2,49 +2,57 @@ library(tidyverse)
 library(ggplot2)
 google_playstore <- read.csv('Google-Playstore.csv')
 
+# Find all categories 
 categories <- google_playstore %>% select(Category) %>% distinct(Category)
-
-google_playstore %>% filter(Category == "Puzzle") %>% select(App.Name)
-
-google_playstore %>% 
-  group_by(Category) %>% 
-  summarize(total_installs = sum(Maximum.Installs))
-
-
-top_100_installed_apps <- google_playstore %>% 
-                            arrange(desc(Maximum.Installs)) %>% 
-                            slice_head(n = 100)
-
-
-
-## Find install total for each category
-app_amount_by_category <- google_playstore %>% 
-  group_by(Category) %>% 
-  summarise(installs = sum(Maximum.Installs))
 
 # Find total installs
 total_installs <- sum(google_playstore$Maximum.Installs)
 
+
+# Find top 100 apps by middle value between max and min installs
+top_100_installed_apps <- google_playstore %>% 
+                            arrange(desc((Maximum.Installs - Minimum.Installs)/2 + Minimum.Installs)) %>% 
+                            slice_head(n = 100)
+
+# Find install total for each category
+app_installs_by_category <- google_playstore %>% 
+  group_by(Category) %>% 
+  summarise(installs = sum(Maximum.Installs))
+
 # Calculate installs as a percentage of total installs
-app_amount_by_category <- app_amount_by_category %>% mutate(perc = (installs/total_installs) * 100)
-app_amount_by_category <- app_amount_by_category %>% mutate(perc = round(perc, 2))
+app_installs_by_category <- app_installs_by_category %>% 
+  mutate(perc = (installs/total_installs) * 100) %>% 
+  mutate(perc = round(perc, 2))
 
-top_5_app_amount_by_category <- app_amount_by_category %>% 
-  mutate(Category = fct_lump_n(Category, 5, other_level="Other")) %>%
-  arrange(desc(installs))
+# Checking if total installs percentage = 100%
+app_installs_by_category %>% summarise(temp = sum(perc))
 
-top_5_app_amount_by_category <- app_amount_by_category %>% 
-  slice_max(order_by = installs, n = 5) %>%
-  mutate(Category = ifelse(row_number() <= 5, as.character(Category), "Other"))
-
-print(top_5_app_amount_by_category)
-  
+# Find top N installed apps by percentage. 
+# Sum categories not in top N.
+# Add Other to the bottom
+top_n_installed_categories <- app_installs_by_category %>% 
+  arrange(desc(perc)) %>%
+  mutate(Category = ifelse(row_number() <= 10, Category, 'Other')) %>%
+  group_by(Category) %>%
+  summarise(perc = sum(perc)) %>%
+  arrange(Category == 'Other', desc(perc))
 
 
 
 # Display as a pie chart
-ggplot(top_5_app_amount_by_category, aes(x="", y=perc, fill=Category)) +
-  geom_bar(stat="identity", width=1, color="white") +
-  coord_polar("y", start=0) +
-  
-  theme_void() # remove background, grid, numeric labels
+ggplot(top_n_installed_categories, aes(x = '', y = perc, fill = Category)) +
+  geom_bar(stat = 'identity', width = 1, color = 'white') +
+  coord_polar('y', start = 0) + 
+  geom_text(aes(label = sprintf('%.0f%%', perc)),
+            position = position_stack(vjust = 0.5),
+            size = 3) +
+  labs(title = 'Top 10 Installed Categories') +
+  theme_void() 
+
+top_n_installed_categories %>% 
+  slice_head(n = 10) %>%
+  ggplot(aes(x = factor(Category, levels = Category), y = perc, fill = factor(Category, levels = Category))) +
+  geom_bar(stat = 'identity', width = 1, color = 'white') +
+  labs(title = 'Top 10 Installed Categories', x = 'Category', y = 'Percentage', fill = 'Category') +
+  theme_minimal() +
+  theme(axis.text.x = element_blank())

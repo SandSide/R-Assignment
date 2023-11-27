9f9efd9f7fb0a98bb200591bb7d10b1bdf3e3a52
SandSide <37347875+SandSide@users.noreply.github.com>
2023-11-27 17:03:21 +0000

    feat: display avg number of installs per category

--- src/R-Assignment/category_installs.R
+++ src/R-Assignment/category_installs.R
@@ -4,7 +4,7 @@ total_installs <- sum(google_playstore$Maximum.Installs)
 # Find install total for each category
 category_installs <- google_playstore %>% 
   group_by(Category) %>% 
-  summarise(installs = sum(Maximum.Installs))
+  summarise(installs = sum(Maximum.Installs), avg_installs = mean(Maximum.Installs), median_installs = median(Maximum.Installs))
 
 # Calculate installs as a percentage of total installs
 category_installs <- category_installs %>% 
@@ -19,6 +19,18 @@ category_installs %>%
   theme_minimal() + 
   guides(fill = 'none')
 
+# Display average installs per category
+category_installs %>% 
+  ggplot(aes(x = avg_installs, y = reorder(Category, avg_installs), fill = Category)) +
+  geom_bar(stat = 'identity', width = 1, color = 'white') +
+  labs(title = 'Average number of installs per category', x = 'Average Number of Installs', y = 'Category', fill = 'Category') +
+  theme_minimal() + 
+  guides(fill = 'none')
+
+
+
 # Summarise data
 category_installs_summary <- category_installs %>%
   summarise(avg_installs = mean(installs), avg_perc = mean(perc), median_installs = median(installs), median_perc = median(perc))
+
+

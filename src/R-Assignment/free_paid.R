3730afcd2767a96afea8c3aeeab8ea1d6fd931cc
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-02 19:52:45 +0000

    feat: find free to paid apps ratio by category

---
+++ src/R-Assignment/free_paid.R
@@ -0,0 +1,20 @@
+
+# Calculate how many free and paids exists, and their ratio the ratio
+free_paid_apps <- google_playstore %>%
+  summarise(paid_apps = sum(Free == 'False'), 
+            free_apps = sum(Free == 'True'), 
+            free_to_paid_ratio = free_apps/paid_apps)
+
+
+free_paid_category <- google_playstore %>%
+  group_by(Category) %>%
+  summarise(paid_apps = sum(Free == 'False'), 
+            free_apps = sum(Free == 'True'), 
+            free_to_paid_ratio = free_apps / paid_apps, 
+            free_perc = (free_apps/n()) * 100,
+            paid_perc = (paid_apps/n()) * 100) %>%
+  arrange(desc(paid_apps))
+
+
+
+

fa42f2f9961ec5ff32f48c6f60fb20ee9c8d3fca
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-03 20:53:20 +0000

    feat: sort Installs+

--- src/R-Assignment/google playstore/category_installs_ranged.R
+++ src/R-Assignment/google playstore/category_installs_ranged.R
@@ -1,68 +1,23 @@
-calculate_difference_from_range <- function(){
+install_ranges <- c('0+', '1+', '5')
+
+app_count_by_installs <- google_playstore %>%
+  group_by(Installs) %>%
+  summarise(total_apps = n()) %>%
+  arrange(Installs)
+
+app_count_by_installs %>%
+  ggplot(aes(y = Installs)) +
+  geom_bar()
+
+# Sort Installs+ based on value
+sorted_install_range <- google_playstore %>% 
+  distinct(Installs) %>%
+  mutate(Installs = gsub('[,+]', '', Installs),
+         Installs = as.numeric(Installs)) %>%
+  arrange(Installs) %>%
+  mutate(Installs = format(Installs, big.mark = ",", scientific = FALSE),
+         Installs =  paste0(Installs, "+"))
   
-  low_installs <- category_installs_ranged %>% 
-    filter(install_range == 'Low') %>%
-    mutate(range_diff = (median_installs/100000) * 100)
   
-  med_installs <- category_installs_ranged %>% 
-    filter(install_range == 'Medium') %>%
-    mutate(range_diff = (median_installs - 100000)/100000 * 100)
-  
-  high_installs <- category_installs_ranged %>% 
-    filter(install_range == 'High') %>%
-    mutate(range_diff = (median_installs - 1000000)/1000000 * 100)
-  
-  very_high_installs <- category_installs_ranged %>% 
-    filter(install_range == 'Very High') %>%
-    mutate(range_diff = (median_installs - 10000000)/10000000 * 100)
-  
-  
-  dfs <- list(low_installs, med_installs, high_installs, very_high_installs)
-  category_installs_ranged <- bind_rows(dfs)
-  
-  return(category_installs_ranged)
-}
-
-category_installs_ranged <- range_categories_by_installs() %>%
-  group_by(Category, install_range) %>%
-    summarise(total_installs = sum(Maximum.Installs),
-              avg_installs = mean(Maximum.Installs),
-              median_installs = median(Maximum.Installs),
-              app_count = n())
-
-
-# Display X range of installed apps
-category_installs_ranged %>% 
-  ggplot(aes(x = median_installs , y = reorder(Category, median_installs), fill = Category)) +
-  geom_bar(stat = 'identity', width = 1, color = 'white') +
-  labs(title = 'Median number of installs per category per install range', x = 'Median Number of Installs', y = 'Category', fill = 'Category') +
-  theme_minimal() + 
-  guides(fill = 'none')
-
-
-category_installs_ranged <- calculate_difference_from_range()
-
-category_installs_ranged %>% ggplot(aes(x = 1, y = range_diff, color = install_range, shape = install_range)) +
-  geom_point(position = position_jitter(width = 1), size = 2) +
-  labs(title = 'Difference between media install and install range as a percentage for each category', x = 'Install Range', y = 'Percentage Diffrence from Median') +
-  theme_minimal() +
-  theme(axis.title.x=element_blank(), axis.text.x = element_blank())
-
-
-
-category_installs_ranged_summary <- category_installs_ranged %>%
-  group_by(install_range) %>%
-  summarise(avg_installs = mean(avg_installs), median_installs = median(median_installs))
-
-
-
-# Frequency of apps by install range
-range_categories_by_installs() %>%
-  ggplot(aes(x = install_range)) +
-  geom_bar() +
-  labs(title = "Distribution of Apps by Installs",
-       x = "Install Range",
-       y = "Frequency") +
-  theme_minimal()
 
 

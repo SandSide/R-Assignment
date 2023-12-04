93e87a2c5a5e4e373350cbe069d18e2ad2ba78f5
SandSide <37347875+SandSide@users.noreply.github.com>
2023-12-04 16:13:02 +0000

    feat: find anomalies in ratings distributions by category

--- src/R-Assignment/google playstore/ratings.R
+++ src/R-Assignment/google playstore/ratings.R
@@ -1,7 +1,7 @@
 summary(google_playstore$Rating)
 
 # Histogram of Ratings
-google_playstore %>%
+rating_dist <- google_playstore %>%
   filter(Rating > 0) %>%
   ggplot(aes(x = Rating)) +
     geom_histogram(binwidth = 0.5, color = 4, fill = 'white') +
@@ -9,3 +9,34 @@ google_playstore %>%
        x = 'Rating',
        y = 'Frequency')
 
+# History of ratings for each category
+category_rating_dist <- google_playstore %>%
+  filter(Rating > 0) %>%
+  ggplot(aes(x = Rating)) +
+  geom_histogram(binwidth = 0.5, color = 4, fill = 'white') +
+  labs(title = 'Distribution of Ratings',
+       x = 'Rating',
+       y = 'Frequency') +
+  facet_wrap(~Category, scales = "free")
+
+
+# Distribution of ratings for casino
+casino_rating_dist <- google_playstore %>%
+  filter(Category == 'Casino',
+         Rating > 0) %>%
+  ggplot(aes(x = Rating)) +
+  geom_histogram(binwidth = 0.5, color = 4, fill = 'white') +
+  labs(title = 'Distribution of Ratings',
+       x = 'Rating',
+       y = 'Frequency')
+  
+# Distribution of ratings for shopping and events categories
+shopping_events_rating_dist <- google_playstore %>%
+  filter(Category == 'Shopping' | Category == 'Events',
+         Rating > 0) %>%
+  ggplot(aes(x = Rating)) +
+  geom_histogram(binwidth = 0.5, color = 4, fill = 'white') +
+  labs(title = 'Distribution of Ratings',
+       x = 'Rating',
+       y = 'Frequency') +
+  facet_wrap(~Category, scales = "free")
